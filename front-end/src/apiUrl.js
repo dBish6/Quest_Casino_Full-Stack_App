38d568978a26b373eeb15d017342fcbfeb022bf1
David <davidbish2002@hotmail.com>
2023-10-15 23:12:26 -0230

    Making room to start v2 of Quest Casino!

--- front-end/src/apiUrl.js
+++
@@ -1,6 +0,0 @@
-const apiURL =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_REST_API_URL_PROD
-    : process.env.REACT_APP_REST_API_URL_DEV;
-
-export default apiURL;
--- front-end/src/hooks/useCache.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { CacheContext } from "../contexts/Cache";
-
-const useCache = () => {
-  return useContext(CacheContext);
-};
-
-export default useCache;
