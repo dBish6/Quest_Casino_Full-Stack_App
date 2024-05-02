e2a8dfa3c6ef377427322ce756efed2cbe6045d7
David <davidbish2002@hotmail.com>
2024-05-02 00:27:59 -0230

    packages; Sheared util added.

---
+++ packages/utils/logger.ts
@@ -0,0 +1,27 @@
+class Logger {
+  disableAll: boolean;
+
+  constructor(disableAll: boolean) {
+    this.disableAll = disableAll;
+  }
+
+  info(message?: any, ...optionalParams: any[]) {
+    if (!this.disableAll && process.env.NODE_ENV !== "production")
+      console.info(message, ...optionalParams);
+  }
+
+  debug(message?: any, ...optionalParams: any[]) {
+    if (!this.disableAll && process.env.NODE_ENV !== "production")
+      console.debug(message, ...optionalParams);
+  }
+
+  warn(message?: any, ...optionalParams: any[]) {
+    if (!this.disableAll) console.warn(message, ...optionalParams);
+  }
+
+  error(message?: any, ...optionalParams: any[]) {
+    if (!this.disableAll) console.error(message, ...optionalParams);
+  }
+}
+
+export default new Logger(false);
