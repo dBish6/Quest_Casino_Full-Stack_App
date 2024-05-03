5456e430f5eb6ef571589ccf3d58f7b19ed12f22
David <davidbish2002@hotmail.com>
2024-05-03 15:41:48 -0230

    Web; Config files updates.

--- apps/web/src/services/firebase.ts
+++ apps/web/src/services/firebase.ts
@@ -1,18 +1,20 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, inMemoryPersistence } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
-const { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID } = process.env;
+const { VITE_FIREBASE_API_KEY, VITE_FIREBASE_AUTH_DOMAIN, VITE_FIREBASE_PROJECT_ID, VITE_FIREBASE_STORAGE_BUCKET, VITE_FIREBASE_MESSAGING_SENDER_ID, VITE_FIREBASE_APP_ID, VITE_FIREBASE_MEASUREMENT_ID } = process.env;
 
-const app = initializeApp({
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID,
-  measurementId: FIREBASE_MEASUREMENT_ID,
-});
+const app = !getApps().length
+  ? initializeApp({
+      apiKey: VITE_FIREBASE_API_KEY,
+      authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+      projectId: VITE_FIREBASE_PROJECT_ID,
+      storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
+      appId: VITE_FIREBASE_APP_ID,
+      measurementId: VITE_FIREBASE_MEASUREMENT_ID,
+    })
+  : getApp();
 
 const auth = getAuth(app);
 // Sets the Persistence to "NONE" because the back-end handles the user session and data.
--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -2,63 +2,42 @@ import { mergeConfig, defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { defaults } from "../../packages/vite-config";
 
-export default mergeConfig(
-  defaults,
-  defineConfig({
-    server: {
-      // middlewareMode: true,
-      // middlewareMode: "ssr",
-      proxy: {
-        // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
-        // "/api": {
-        //   target: "http://jsonplaceholder.typicode.com",
-        //   changeOrigin: true,
-        //   rewrite: (path) => path.replace(/^\/api/, ""),
-        // },
-        // Using the proxy instance
-        "/api": {
-          target: "http://jsonplaceholder.typicode.com",
-          changeOrigin: true,
-          configure: (proxy, options) => {
-            // proxy will be an instance of 'http-proxy'
+export default defineConfig(({ mode }) =>
+  mergeConfig(
+    defaults,
+    defineConfig({
+      server: {
+        port: 3000,
+        proxy: {
+          // with options: http://localhost:5173/api/bar-> http://jsonplaceholder.typicode.com/bar
+          // "/api": {
+          //   target: "http://jsonplaceholder.typicode.com",
+          //   changeOrigin: true,
+          //   rewrite: (path) => path.replace(/^\/api/, ""),
+          // },
+          // Using the proxy instance
+          "/api": {
+            target: "http://jsonplaceholder.typicode.com",
+            changeOrigin: true,
+            configure: (proxy, options) => {
+              // proxy will be an instance of 'http-proxy'
+            },
           },
         },
       },
-    },
-    ssr: {},
-    css: {
-      modules: {
-        localsConvention: "camelCaseOnly",
-        generateScopedName: "[local]_[hash:base64:2]",
+      css: {
+        modules: {
+          localsConvention: "camelCaseOnly",
+          generateScopedName:
+            mode === "production"
+              ? "[hash:base64:2]"
+              : "[local]_[hash:base64:2]",
+        },
       },
-    },
-    // css: {
-    //   // postcss: postcssConfig,
-    //   modules: {
-    //     localsConvention: "camelCaseOnly",
-    //     // generateScopedName: import.meta.env.PROD
-    //     //   ? "[hash:base64:2]"
-    //     //   : "[local]_[hash:base64:2]",
-    //   },
-    // },
-    plugins: [react()],
-    build: {
-      outDir: "build/client",
-    },
-    // build: {
-    //   target: "node", // Set the build target to Node.js
-    //   outDir: "build", // Output directory for the built files
-    //   rollupOptions: {
-    //     input: {
-    //       main: path.resolve(__dirname, "./app/front-end/src/index.html"), // Adjust the path to your entry point
-    //     },
-    //     // Externalize dependencies for SSR
-    //     external: ["react", "react-dom", "react-router-dom"],
-    //   },
-    // },
-    // optimizeDeps: {
-    //   // Explicitly specify the dependencies needed for SSR
-    //   include: ["react", "react-dom", "react-router-dom"],
-    // },
-  })
+      plugins: [react()],
+      build: {
+        outDir: "build/client",
+      },
+    })
+  )
 );
