6c980061eaf394314a41d1c78fa47c251a06f505
David <davidbish2002@hotmail.com>
2024-05-28 15:06:42 -0230

    web; Small storybook fix.

--- apps/web/.storybook/mockRouter.tsx
+++ apps/web/.storybook/mockRouter.tsx
@@ -1,23 +1,12 @@
 import React from "react";
 import { createMemoryRouter } from "react-router-dom";
 
-import registerAction from "../src/features/auth/actions/register";
-
 export default function mockRouter(Story: any) {
   return createMemoryRouter(
     [
       {
         path: "*",
         element: <Story />,
-      },
-      {
-        path: "/action",
-        children: [
-          {
-            path: "register",
-            action: registerAction,
-          },
-        ],
       },
     ],
     {
