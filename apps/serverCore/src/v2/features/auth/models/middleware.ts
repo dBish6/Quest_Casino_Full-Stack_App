a80855db28245a69bd5676509d8055a5a15a95cb
David <davidbish2002@hotmail.com>
2024-05-31 19:58:36 -0230

    serverCore; Username is already taken mongoose middleware.

--- apps/serverCore/src/v2/features/auth/models/middleware.ts
+++ apps/serverCore/src/v2/features/auth/models/middleware.ts
@@ -13,17 +13,22 @@ interface SharedDocument extends Document {
   updated_at: Date;
 }
 
+interface MongoError extends Error {
+  code: number;
+  keyPattern: any;
+}
+
 function commonMiddleware(next: CallbackWithoutResultAndOptionalError) {
   // @ts-ignore
   const doc = this as SharedDocument;
-
   if (!doc.isNew) {
     if (doc.isModified("_id")) {
       throw createApiError(
-        null,
+        Error(
+          `Unexpected change occurred within a document:\n${JSON.stringify(doc.getChanges(), null, 2)}`
+        ),
         "commonMiddleware error.",
-        400,
-        "Unexpected change occurred within a document"
+        400
       );
     }
   }
@@ -31,9 +36,28 @@ function commonMiddleware(next: CallbackWithoutResultAndOptionalError) {
   next();
 }
 
-// FIXME: I want this middleware to run on every operation, but this is probably not needed...
 [userSchema, userStatisticsSchema, userActivitySchema].forEach(
   (schema: Schema<any, any>) => {
-    schema.pre("validate", commonMiddleware);
+    schema.pre(["save", "validate"], commonMiddleware);
   }
 );
+
+function handleUniqueUsername(
+  monError: Error,
+  doc: any,
+  next: CallbackWithoutResultAndOptionalError
+) {
+  const error = monError as MongoError;
+  if (error.code === 11000 && error.keyPattern?.username) {
+    throw createApiError(
+      Error("Username is already taken. Please try a different username."),
+      "handleUniqueUsername error.",
+      400
+    );
+  }
+
+  next();
+}
+
+userSchema.post("save", { errorHandler: true }, handleUniqueUsername);
+userSchema.post("updateOne", { errorHandler: true }, handleUniqueUsername);
