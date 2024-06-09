9f42e5379e8b67e22dfd78688bdd0aaffbe82652
David <davidbish2002@hotmail.com>
2024-06-09 00:42:22 -0230

    web; Storybook mock user.

--- apps/web/src/features/auth/redux/authSelectors.ts
+++ apps/web/src/features/auth/redux/authSelectors.ts
@@ -1,4 +1,6 @@
 import type { RootState } from "@redux/store";
 
-export const selectUser = (state: RootState) => state.auth.user,
+export const selectUserCredentials = (state: RootState) =>
+    state.auth.user.credentials,
+  selectUserCsrfToken = (state: RootState) => state.auth.user.token.csrf,
   selectUserOStateToken = (state: RootState) => state.auth.user.token.oState;
