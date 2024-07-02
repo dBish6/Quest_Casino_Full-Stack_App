aebb5bad4addab4cfe874d07fba82113c930cb12
David <davidbish2002@hotmail.com>
2024-07-01 23:59:45 -0230

    web; Massive improvement to socket.io client setup.

--- apps/web/src/components/loaders/ResourceLoader.tsx
+++ apps/web/src/components/loaders/ResourceLoader.tsx
@@ -1,63 +1,63 @@
 import type { FeatureBundle } from "framer-motion";
-import type { Socket } from "socket.io-client";
+import type { TimeoutObj } from "@services/socket";
 
 import { useRef, useState, useLayoutEffect } from "react";
 
-import { logger } from "@qc/utils";
 import delay from "@utils/delay";
-import { history } from "@utils/History";
 
-import { instance } from "@services/socket";
+import { useAppSelector } from "@redux/hooks";
+import { selectUserCsrfToken } from "@authFeat/redux/authSelectors";
+
+import { socketInstancesConnectionProvider } from "@services/socket";
 
 import OverlayLoader from "./overlay/OverlayLoader";
 
-export default function ResourceLoader({
-  children,
-}: React.PropsWithChildren<{}>) {
+export default function ResourceLoader({ children }: React.PropsWithChildren<{}>) {
   const FramerFeatureBundleRef = useRef<{
       LazyMotion?: React.ElementType;
       domMax?: FeatureBundle;
     }>({}),
     [progress, setProgress] = useState({
-      loading: false, // I would love to show the loader initially but the portal in the loader breaks hydration.
-      message: "Loading animating magic..",
+      loading: false, // I would love to show the loader initially but the portal in OverlayLoader breaks hydration.
+      message: "Loading animating magic...",
     });
+
+  const userToken = useAppSelector(selectUserCsrfToken);
 
   if (typeof window !== "undefined") {
     useLayoutEffect(() => {
       if (!progress.loading) {
         setProgress((prev) => ({ ...prev, loading: true }));
-        const intervalArr: NodeJS.Timeout[] = [];
+        const timeoutObj: TimeoutObj = {};
 
         (async () => {
           try {
-            const { LazyMotion, domMax } = await import("framer-motion");
+            if (!FramerFeatureBundleRef.current.LazyMotion || !FramerFeatureBundleRef.current.domMax) {
+              const { LazyMotion, domMax } = await import("framer-motion");
 
-            FramerFeatureBundleRef.current = { LazyMotion, domMax };
-            await delay(500);
+              FramerFeatureBundleRef.current = { LazyMotion, domMax };
+              await delay(1500);
+            }
 
-            setProgress((prev) => ({
-              ...prev,
-              message: "Syncing the matrix...",
-            }));
-            await Promise.all(
-              Object.entries(instance).map(([namespace, instance]) =>
-                checkSocketConnection(namespace, instance, intervalArr)
-              )
-            );
+            setProgress((prev) => ({ ...prev, message: "Syncing You with Others..." }));
+            if (userToken) await socketInstancesConnectionProvider(timeoutObj)
+
+            delay(30000, () => {
+              if (progress.loading)
+                setProgress((prev) => ({ ...prev, message: "Taking longer than expected..." }));
+            });
           } catch (error: any) {
             console.error("Loading resources error:\n", error.message);
-            history.push("/error-500");
+            // history.push("/error-500");
           } finally {
             setProgress((prev) => ({ ...prev, loading: false }));
           }
         })();
 
-        return () => {
-          if (intervalArr.length) intervalArr.forEach(clearInterval);
-        };
+        return () =>
+          Object.values(timeoutObj).forEach((timeout) => clearTimeout(timeout));
       }
-    }, []);
+    }, [userToken]);
   }
 
   const { LazyMotion, domMax } = FramerFeatureBundleRef.current;
@@ -73,33 +73,4 @@ export default function ResourceLoader({
       )}
     </>
   );
-}
-
-async function checkSocketConnection(
-  namespace: string,
-  instance: Socket,
-  intervalArr: NodeJS.Timeout[]
-) {
-  return new Promise<void>((resolve, reject) => {
-    let retries = instance.io.opts.reconnectionAttempts!;
-
-    if (instance.connected) {
-      resolve();
-    } else {
-      const interval = setInterval(() => {
-        if (retries === 0) {
-          clearInterval(interval);
-          reject(new Error(`Failed to establish a stable connection with ${namespace} socket instance.`)); // prettier-ignore
-        }
-        if (instance.connected) resolve(clearInterval(interval));
-
-        logger.debug(
-          `${namespace} socket instance connection status check failed; ${retries} retries left.`
-        );
-        retries--;
-      }, 6000);
-
-      intervalArr.push(interval);
-    }
-  });
 }
--- apps/web/src/services/socket.ts
+++ apps/web/src/services/socket.ts
@@ -1,44 +1,141 @@
-import { type ManagerOptions, type SocketOptions, io } from "socket.io-client";
+import type { Socket } from "socket.io-client";
+import type { SocketResponse, ErrorSocketResponse } from "@typings/ApiResponse";
+
+import { io } from "socket.io-client";
 import { logger } from "@qc/utils";
+import { history } from "@utils/History";
+
+export const namespaces = ["auth", "chat"] as const;
+export type SocketNamespaces = (typeof namespaces)[number];
+export type TimeoutObj = Partial<Record<SocketNamespaces, NodeJS.Timeout>>;
 
 // const baseUrl = "/socket/v2",
-const baseUrl = "http://localhost:4000/socket/v2",
-  options: Partial<ManagerOptions & SocketOptions> = {
-    // port: 4000,
-    reconnectionAttempts: 8,
-    reconnectionDelay: 3500,
-    withCredentials: true,
-  };
+const baseUrl = "http://localhost:4000/socket/v2";
+// const baseUrl = ":4000/socket/v2",
 
-export const instance = {
-  auth: io(`${baseUrl}/auth`, options),
-  chat: io(`${baseUrl}/chat`, options),
-};
+/**
+ * Creates a socket instance only once for a specific namespace.
+ */
+export function getSocketInstance(namespace: SocketNamespaces) {
+  let socket: Socket;
 
-const setupSockets = () => {
-  for (const [namespace, inst] of Object.entries(instance)) {
-    const engine = inst.io.engine,
-      engineLogs = true;
-
-    inst.on("connect_error", async (error) => {
-      console.error({
-        info: "Socket connection error attempting to retry connection...",
-        ERROR: error.message,
-      });
-    });
-    inst.on("connect", () => {
-      console.log(`Connection established for ${namespace} socket; ${inst.id}`);
-    });
-
-    if (import.meta.env.MODE !== "production" && engineLogs) {
-      engine.on("packet", ({ type, data }) => {
-        logger.debug("Received:", { type: type, data: data }); // Received
-      });
-      engine.on("packetCreate", ({ type, data }) => {
-        logger.debug("Sent:", { type: type, data: data }); // Sent
+  const getSocket = () => {
+    if (!socket) {
+      socket = io(`${baseUrl}/${namespace}`, {
+        autoConnect: false,
+        reconnection: false,
+        transports: ["websocket", "polling"],
+        withCredentials: true,
       });
     }
-  }
-};
 
-export default setupSockets;
+    return socket;
+  };
+
+  return getSocket();
+}
+
+export function emitAsPromise(socket: Socket) {
+  return <TData extends object>(event: string, data: TData): Promise<any> => {
+    return new Promise((resolve, reject) => {
+      socket.emit(event, data, (res: SocketResponse<TData>) => {
+        if (res.status !== "ok") {
+          reject(res);
+        } else {
+          resolve(res);
+        }
+      });
+    });
+  };
+}
+
+/**
+ * Establishes connections to the host for all socket instances and handles retries up to 9 times if the connection fails. Ensures all socket
+ * instances are set up appropriately.
+ */
+export async function socketInstancesConnectionProvider(timeoutObj: TimeoutObj) {
+  const connections = namespaces.map(async (namespace) => {
+    const socket = getSocketInstance(namespace);
+
+    return new Promise<{ socket: Socket; namespace: SocketNamespaces }>((resolve, reject) => {
+        let retries = 9;
+
+        if (socket.connected) {
+          resolve({ socket, namespace });
+        } else {
+          socket
+            .on("connect", () => {
+              logger.debug(`Connection established for ${namespace} socket; ${socket.id}`);
+
+              socket.removeAllListeners();
+              resolve({ socket, namespace });
+            })
+            .on("connect_error", (error) => {
+              logger.error(`${namespace} socket instance connection error:\n`, error.message);
+
+              const timeout = setTimeout(() => attemptConnection(), 5000);
+              timeoutObj[namespace] = timeout;
+            });
+
+          const attemptConnection = () => {
+            retries--;
+            logger.debug(
+              `${namespace} socket instance attempting to establish a connection; ${retries} retries left...`
+            );
+            socket.connect();
+
+            if (retries === 0) {
+              socket.removeAllListeners();
+              reject(
+                new Error(
+                  `Failed to establish a stable connection with ${namespace} socket instance.`
+                )
+              );
+            }
+          };
+          attemptConnection();
+        }
+      }
+    ).then(({ socket, namespace }) => setupDefaultListeners(socket, namespace));
+  });
+
+  await Promise.all(connections);
+}
+
+function setupDefaultListeners(socket: Socket, namespace: SocketNamespaces) {
+  const engine = socket.io.engine,
+    engineLogs = true;
+
+  socketErrorHandler(socket, namespace);
+
+  if (import.meta.env.MODE !== "production" && engineLogs) {
+    engine
+      .on("packet", ({ type, data }) => 
+        logger.debug(`${namespace} received:`, { type: type, data: data })
+      )
+      .on("packetCreate", ({ type, data }) =>
+        logger.debug(`${namespace} Sent:`, { type: type, data: data })
+      );
+  }
+}
+function socketErrorHandler(socket: Socket, namespace: SocketNamespaces) {
+  socket
+    .on("connect_error", (error) => 
+      logger.error(
+        `Unexpected error occurred from ${namespace} socket instance:\n`, error.message
+      )
+    )
+    .on("error", (error: ErrorSocketResponse) => {
+      // Not sure how I want to do this yet.
+      switch (error.status) {
+        case "bad request":
+          error.message.includes("leave", -1) && history.push("/error-500");
+          break;
+        case "internal error":
+          history.push("/error-500");
+          break;
+        default:
+          break;
+      }
+    });
+}
