d1f8433d024bcc6565224d4b6b4003519df49627
David <davidbish2002@hotmail.com>
2024-09-24 16:52:00 -0230

    web; Final iteration the image component, lazy loading fix.

--- apps/web/src/components/common/image/Image.tsx
+++ apps/web/src/components/common/image/Image.tsx
@@ -1,9 +1,9 @@
-import { forwardRef, useRef } from "react";
+import { forwardRef } from "react";
 
 import noImage from "/images/no-image.webp"
 import s from "./image.module.css";
 
-export interface ImageProps extends React.ComponentProps<"img"> {
+export interface ImageProps extends Omit<React.ComponentProps<"img">, "loading" | "aria-busy"> {
   src: string;
   alt: string;
   load?: boolean;
@@ -18,43 +18,34 @@ export interface ImageProps extends React.ComponentProps<"img"> {
 
 const Image = forwardRef<HTMLImageElement, ImageProps>(
   ({ src, alt, load = true, size, fill, ...props }, ref) => {
-    const containerRef = useRef<HTMLDivElement>(null);
+    const handleLazyLoad = (img: HTMLImageElement) => {
+      if (load && img.getAttribute("data-loaded") !== "true") {
+        const complete = () => {
+          img.parentElement!.setAttribute("data-loaded", "true");
+          img.setAttribute("aria-busy", "false");
 
-    const handleImageLazyLoad = (img: HTMLImageElement) => {
-      if (load) {
-        let timeout: NodeJS.Timeout;
+          img.onload = null;
+          img.onerror = null;
+        }
 
-        const cleanUp = () => {
-          if (img) {
-            img.removeEventListener("load", handleLoad);
-            img.removeEventListener("error", handleError);
+        if (img.complete) {
+          complete();
+        } else {
+          img.onload = () => {
+            console.log("img onload", img.src);
+            complete();
           }
-          clearTimeout(timeout);
-        };
-
-        const handleLoad = () => {
-          if (containerRef.current) containerRef.current.setAttribute("data-loaded", "true");
-          cleanUp();
-        };
-  
-        const handleError = () => {
-          if (img) img.src = noImage;
-          cleanUp();
-        };
-  
-        timeout = setTimeout(handleError, 15000);
-
-        if (img.complete) handleLoad();
-        else {
-          img.addEventListener("load", handleLoad);
-          img.addEventListener("error", handleError);
+          img.onerror = () => {
+            console.log("img onerror", img.src);
+            img.src = noImage;
+            complete();
+          }
         }
       }
     }
 
     return (
       <div
-        ref={containerRef}
         role="presentation"
         className={`${s.container}${load ? " " + s.load : ""}`}
         style={{
@@ -78,15 +69,16 @@ const Image = forwardRef<HTMLImageElement, ImageProps>(
             if (typeof ref === "function") ref(node);
             else if (ref) ref.current = node;
 
-            if (node) handleImageLazyLoad(node);
+            if (node) handleLazyLoad(node);
           }}
-          {...(load && { loading: "lazy" })}
           src={src}
           alt={alt}
           {...props}
+
+          {...(load && { "aria-busy": "true", loading: "lazy" })}
           style={{
             ...(fill
-              ? { width: "100%", height: "100%" }
+              ? { width: "100%", height: "100%", objectFit: "cover" }
               : size &&
                 Object.keys(size).length && {
                   ...(size.width && {
