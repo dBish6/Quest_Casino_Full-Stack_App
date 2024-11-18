3dd9ab3eb4bc5c468ca9c759fa413f404e141b18
David <davidbish2002@hotmail.com>
2024-11-17 23:21:35 -0330

    web; validateRegister deleted and replaced with validateUser for all user forms.

--- apps/web/src/features/auth/actions/validateUser.ts
+++ apps/web/src/features/auth/actions/validateUser.ts
@@ -1,12 +1,14 @@
 import type { ActionFunction } from "react-router-dom";
-import type RegisterBodyDto from "@qc/typescript/dtos/RegisterBodyDto";
 
 import { json } from "react-router-dom";
 import { logger, capitalize, validateEmail } from "@qc/utils";
 
-const optionalFields = new Set(["region", "calling_code", "phone_number"]);
+const optionalFields = new Set(["region", "calling_code", "phone_number"]); // For register. For anything other then profile.
 
-const validateRegisterAction: ActionFunction = async ({ request }) => {
+/**
+ * Validates form fields in user forms (register, update profile, reset password, forgot password).
+ */
+const validateUserAction: ActionFunction = async ({ request }) => {
   try {
     const formData = await request.formData(),
       result = validate(formData);
@@ -14,22 +16,27 @@ const validateRegisterAction: ActionFunction = async ({ request }) => {
     if (Object.keys(result.errors).length) return json({ errors: result.errors }, { status: 400 });
     else return json({ reqBody: result.reqBody }, { status: 200 });
   } catch (error: any) {
-    logger.error("registerAction error:\n", error.message);
-    return json({ ERROR: error.message }, { status: 500 })
+    logger.error("validateUser error:\n", error.message);
+    return json({ ERROR: error.message }, { status: 500 });
   }
 };
-export default validateRegisterAction;
+export default validateUserAction;
 
 function validate(formData: FormData) {
   if (formData.get("bot")!.toString().length) return { errors: { bot: "Access Denied" } };
   formData.delete("bot");
 
+  const isProfile = formData.get("isProfile");
+  formData.delete("isProfile");
+
   const errors: Record<string, string> = {},
-  reqBody: RegisterBodyDto = {} as RegisterBodyDto;
+    reqBody: Record<string, string | { old: string, new: string }> = {};
 
   for (const [key, value] of formData.entries()) {
     if (key === "calling_code") continue;
-    const fieldValue = value.toString();
+
+    const fieldValue = value.toString(),
+      isPassword = ["old_password", "new_password"].includes(key);
 
     if (fieldValue.length) {
       const errorMsg = validateField(key, fieldValue, formData);
@@ -39,19 +46,22 @@ function validate(formData: FormData) {
           errors.con_password = errorMsg;
         } else {
           errors[
-            key === "phone_number" && errorMsg.includes("calling code", -1) ? "calling_code" : key
+            key === "phone_number" && errorMsg.includes("calling code", -1) ? "calling_code"
+              : key === "new_password" && errorMsg.endsWith("current password.") ? "old_password"
+              : key
           ] = errorMsg;
         }
-      } else {
-        // Forms the request body to be sent in the register request.
-        reqBody[key as keyof RegisterBodyDto] =
-          key === "phone_number" ? `${formData.get("calling_code")} ${fieldValue}` : fieldValue;
+      } else if (key !== "con_password" && !isPassword) {
+        // Forms the correct request body.
+        reqBody[key] = key === "phone_number" ? `${formData.get("calling_code")} ${fieldValue}` : fieldValue;
       }
-    } else if (!optionalFields.has(key)) {
+    } else if (((isProfile && isPassword) || (!isProfile && !optionalFields.has(key)))) {
       errors[key] =
         key === "con_password"
           ? "Please confirm your password."
-          : `${capitalize(key)} is required.`;
+          : key === "old_password"
+            ? "Your current password is required."
+            : `${capitalize(key)} is required.`;
     }
   }
 
@@ -64,7 +74,8 @@ function validateField(key: string, value: string, formData: FormData) {
     case "email":
       return validateEmail(value);
     case "password":
-      return validatePassword(value);
+    case "new_password":
+      return validatePassword(key, value);
     case "con_password":
       return confirmPassword(value, formData.get("password")!.toString());
     case "phone_number":
@@ -89,12 +100,15 @@ function validateUsername(username: string) {
  * - Min of 6 characters.
  * - Max of 128 characters.
  * - At least one uppercase letter.
+ * 
+ * Also, checks their old password for password resets.
  */
-export function validatePassword(password: string) {
-  if (password.length < 6) return "Password must be at least 6 characters.";
+export function validatePassword(key: string, password: string) {
+  // if (oldPassword && !oldPassword.toString().length) return "Please enter your current password.";
+  if (password.length < 6) return `${capitalize(key)} must be at least 6 characters.`;
   else if (password.length > 128) return "Max of 128 characters exceeded.";
   else if (!/[A-Z]/.test(password))
-    return "Password must contain at least one capital letter.";
+    return `${capitalize(key)} must contain at least one capital letter.`;
 }
 export function confirmPassword(conPassword: string, password: string) {
   if (conPassword !== password) return "Passwords do not match.";
