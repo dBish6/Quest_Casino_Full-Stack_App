73c163b103497f794ee861ab2cdbdd70525e3cd1
David <davidbish2002@hotmail.com>
2025-03-08 16:48:05 -0330

    serverCore; Fixed how emails were not in the build.

--- apps/serverCore/rollup.config.js
+++ apps/serverCore/rollup.config.js
@@ -4,6 +4,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import autoExternal from "rollup-plugin-auto-external";
 import terser from "@rollup/plugin-terser";
+import copy from "rollup-plugin-copy";
 import { visualizer } from "rollup-plugin-visualizer";
 
 export default {
@@ -22,6 +23,21 @@ export default {
     commonjs(),
     autoExternal(),
     terser(),
+    copy({
+      targets: [
+        {
+          src: "src/v2/features/auth/http/emails/templates/**/*",
+          dest: "build/features/auth/http/emails/templates",
+          rename: (_, _, fullPath) =>
+            fullPath.replace(/^src\/v2\/features\/auth\/http\/emails\/templates\//, "")
+        },
+        {
+          src: "src/v2/emails/partials/**/*",
+          dest: "build/emails/partials"
+        }
+      ],
+      copyOnce: true
+    }),    
     visualizer()
   ]
 };
